77032bf334d2880c5b37b5e8d4542f1317ad5fcd
maarjaroots <maarja.roots@gmail.com>
2021-01-12 17:07:03 +0000

    Link to location page to go bact to the about page

--- src/pages/{contentfulAbout.name}.js
+++ src/pages/{contentfulAbout.name}.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
+import "../components/location.css"
 import { graphql, Link } from "gatsby";
 
 export default function about(props) {
--- src/pages/{contentfulLocation.name}.js
+++ src/pages/{contentfulLocation.name}.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Layout from "../components/layout";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
+
+const ListLink = (props) => (
+  <li>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+);
 
 export default function location(props) {
   return (
     <Layout>
       <h1>Page of:</h1>
-
       <h2> {props.data.contentfulLocation.name} </h2>
       <h3> {props.data.contentfulLocation.title} </h3>
+      <div className="location">
+        <ListLink to="/about">
+          Back to: {props.data.contentfulAbout.name}
+        </ListLink>
+      </div>
     </Layout>
   );
 }
@@ -19,6 +29,9 @@ export const query = graphql`
       id
       name
       title
+    }
+    contentfulAbout {
+      name
     }
   }
 `;
